19e006bafd0138085862a418891d8dcaf4dfff15
solaretech <solaretech.kk@gmail.com>
2020-03-22 19:55:30 +0900

    ToDo機能のベース完成

---
+++ components/ToDoList.vue
@@ -0,0 +1,33 @@
+<template>
+	<v-card>
+		<v-card-title>
+			ToDo List
+			<v-spacer></v-spacer>
+			<v-text-field v-model="search" append-icon="search" label="Search" single-line hide-details></v-text-field>
+		</v-card-title>
+		<v-data-table :headers="headers" :items="todos" :search="search"></v-data-table>
+	</v-card>
+</template>
+
+<script>
+export default {
+  props: ['todos'],
+  data () {
+    return {
+      search: '',
+      headers: [
+        {
+          text: 'タイトル',
+          align: 'left',
+          sortable: false,
+          value: 'title'
+        },
+        { text: 'ユーザー名', value: 'username' }
+      ]
+    }
+  }
+}
+</script>
+
+<style>
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,33 @@
+<template>
+  <div>
+    <AddToDo @submit="addToDo"  />
+    <TodoList :todos="todos" />
+  </div>
+</template>
+
+<script>
+import AddToDo from '@/components/AddTodo'
+import TodoList from '@/components/TodoList'
+
+export default {
+  components: {
+    AddToDo,
+    TodoList
+  },
+  data () {
+    return {
+      todos: []
+    }
+  },
+  methods: {
+    addToDo (title) {
+      this.todos.push({
+        title
+      })
+    }
+  }
+}
+</script>
+
+<style>
+</style>
